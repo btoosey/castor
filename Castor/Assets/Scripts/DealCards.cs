6526f8ac65e275f304fbd1f842f908f0464e8f99
Ben Toosey <41492238+btoosey@users.noreply.github.com>
2021-10-20 15:31:51 +0100

    ADD: Deal Player 1 cards
    Currently instantiating 4 cards for player 1, but not in correct location, and instantiating clones of already made card, not from SO.

---
+++ Castor/Assets/Scripts/DealCards.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DealCards : MonoBehaviour
+{
+
+    public GameObject Card;
+    public GameObject PlayerArea;
+
+    public void OnClick()
+	{
+		for (int i = 0; i < 4; i++)
+		{
+            GameObject card = Instantiate(Card, new Vector2(0, 0), Quaternion.identity);
+            card.transform.SetParent(PlayerArea.transform, false);
+		}
+	}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
